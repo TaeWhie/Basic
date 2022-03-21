f7c4fab0fe58da9c1177099d191a4d3f67efc49d
TaeWhie <xognl427@naver.com>
2022-03-21 19:07:56 +0900

    TRS

--- Assets/Code/PolygonMaker.cs
+++ Assets/Code/PolygonMaker.cs
@@ -8,9 +8,42 @@ public class PolygonMaker : MonoBehaviour
     void Start()
     {
         //MakePolygon(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1));
-       // MakeQuad(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1));
-        MakeSubmeshQuad(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1));
+        // MakeQuad(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1));
+        //MakeSubmeshQuad(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1));
 
+        //TRS
+        //quad object size 
+        const float width = 1;
+        const float height = 1;
+        //gameObject 생성
+        Mesh mesh = MakeQuad(new Vector3(-width / 2, 0, -height / 2), new Vector3(-width / 2, 0, height / 2), new Vector3(width / 2, 0, -height / 2), new Vector3(width / 2, 0, height / 2)).GetComponent<MeshFilter>().mesh;
+        GameObject go = new GameObject("extruded Mesh");
+        Material mt = new Material(Shader.Find("Standard"));
+        go.AddComponent<MeshFilter>();
+        go.AddComponent<MeshRenderer>().material = mt;
+
+        //변환값을 저장할 새로운 위치 리스트
+        List<Vector3> newVert = new List<Vector3>();
+
+        //mesh의 vertices의 갯수 만큼 반복
+        foreach (Vector3 v in mesh.vertices)
+        {
+            Matrix4x4 fx;
+            Matrix4x4 fxT;
+            Matrix4x4 fxR;
+            Matrix4x4 fxS;
+
+            //변환행렬곱 수행 
+            fxT = Translate(new Vector3(2f, 0f, 0f));
+            fxR = Rotate(new Vector3(45f, 0f, 0f));
+            fxS = Scale(new Vector3(0.5f, 2f, 2f));
+            //변환행렬 합성 (x축으로 2만큼 평행이동, x축기준 45도 회전, x축기준 0.5배 scale)
+            fx = fxT * fxR * fxS;
+            newVert.Add(fx.MultiplyPoint(v));//벡터를 정점(x,y,z,w)으로서 곱한다는 의미이다.
+           // newVert.Add(fx*v); 이렇게 하게되면 44행렬과 벡터3을 곱하게 됨으로 4열을 이용하는 transform의 변환겂을 얻을 수 없다.
+        }
+        //mesh의 위치정보 갱신
+        mesh.vertices = newVert.ToArray();
     }
 
     // Update is called once per frame
@@ -92,5 +125,55 @@ public class PolygonMaker : MonoBehaviour
         mf.mesh = mesh;
         mr.materials = mts;
         return go;
+    }
+    //vector의 이동
+    public static Matrix4x4 Translate(Vector3 aPosition)
+    {
+        //Matrix4x4.identity;는 단위 행렬을 의미한다.
+        var m = Matrix4x4.identity; // 1   0   0   x
+        m.m03 = aPosition.x;        // 0   1   0   y
+        m.m13 = aPosition.y;        // 0   0   1   z
+        m.m23 = aPosition.z;        // 0   0   0   1
+        return m;
+    }
+    //vector의 회전
+    public static Matrix4x4 RotateX(float aAngleRad)
+    {
+        Matrix4x4 m = Matrix4x4.identity;     //  1   0   0   0 
+        m.m11 = m.m22 = Mathf.Cos(aAngleRad); //  0  cos -sin 0
+        m.m21 = Mathf.Sin(aAngleRad);         //  0  sin  cos 0
+        m.m12 = -m.m21;                       //  0   0   0   1
+        return m;
+    }
+    public static Matrix4x4 RotateY(float aAngleRad)
+    {
+        Matrix4x4 m = Matrix4x4.identity;     // cos  0  sin  0
+        m.m00 = m.m22 = Mathf.Cos(aAngleRad); //  0   1   0   0
+        m.m02 = Mathf.Sin(aAngleRad);         //-sin  0  cos  0
+        m.m20 = -m.m02;                       //  0   0   0   1
+        return m;
+    }
+    public static Matrix4x4 RotateZ(float aAngleRad)
+    {
+        Matrix4x4 m = Matrix4x4.identity;     // cos -sin 0   0
+        m.m00 = m.m11 = Mathf.Cos(aAngleRad); // sin  cos 0   0
+        m.m10 = Mathf.Sin(aAngleRad);         //  0   0   1   0
+        m.m01 = -m.m10;                       //  0   0   0   1
+        return m;
+    }
+    //최종 회전 결과
+    public static Matrix4x4 Rotate(Vector3 aEulerAngles)
+    {
+        var rad = aEulerAngles * Mathf.Deg2Rad;//오일러 값으로 받은 것을 라디안으로 변환 
+        return RotateY(rad.y) * RotateX(rad.x) * RotateZ(rad.z);
+    }
+    //vector의 확대/축소
+    public static Matrix4x4 Scale(Vector3 aScale)
+    {
+        var m = Matrix4x4.identity; //  sx   0   0   0
+        m.m00 = aScale.x;           //   0  sy   0   0
+        m.m11 = aScale.y;           //   0   0  sz   0
+        m.m22 = aScale.z;           //   0   0   0   1
+        return m;
     }
 }
